2b73b5f2843c52b82acca3e350c19344599304cf
Muen Zhang <75929368+zmuen@users.noreply.github.com>
2024-03-17 18:03:41 -0400

    Update 03_parse.py

--- 03_parse.py
+++ 03_parse.py
@@ -3,16 +3,6 @@
 # 
 #   Objective: Parse course data leveraging
 #   HTML elements structure.
-# 
-#   Tools/Resources: Use resources like the 
-#   DOMParser, BeautifulSoup, or Regular Expressions.
-#       Beautiful Soup:
-#           https://www.crummy.com/software/BeautifulSoup/
-#       DOMParser:
-#           https://developer.mozilla.org/en-US/docs/Web/API/DOMParser
-#       RegEx:
-#           https://regexr.com 
-#           https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_expressions
 # -----------------------------------------------
 
 from bs4 import BeautifulSoup
