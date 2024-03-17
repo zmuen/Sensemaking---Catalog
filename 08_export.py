0ded17779065a9df42e2fbe8775bf081388f65d9
Muen Zhang <75929368+zmuen@users.noreply.github.com>
2024-03-17 18:05:11 -0400

    Update 08_export.py

--- 08_export.py
+++ 08_export.py
@@ -1,22 +1,6 @@
 # -----------------------------------------------
 #  8. Export a Clean Formatted Dataset
 #  of the Entire University Catalog:
-# 
-#  Export a Clean Formatted Dataset of 
-#  the Entire University Catalog: The 
-#  dataset you would have liked when you 
-#  started. Prepare and export a clean, 
-#  well-formatted dataset encompassing 
-#  the entire university catalog. This 
-#  dataset should be in a form that is 
-#  readily usable for analysis and 
-#  visualization, reflecting the cleaned 
-#  and consolidated data you've worked 
-#  with throughout the project. Document 
-#  the structure of your dataset, including 
-#  a description of columns, data types, and 
-#  any assumptions or decisions made during 
-#  the data preparation process.
 # -----------------------------------------------
 
 import json
