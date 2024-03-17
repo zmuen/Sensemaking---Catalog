1e9e303d633b0e1314287d8d4b26cf51e4d757e1
Muen Zhang <75929368+zmuen@users.noreply.github.com>
2024-03-17 18:05:24 -0400

    Update 09_pipeline.py

--- 09_pipeline.py
+++ 09_pipeline.py
@@ -1,14 +1,5 @@
 # -----------------------------------------------
 #  9. Data pipeline:
-# 
-#  Write a program that automates the 
-#  sequential execution of previously created 
-#  script files, ensuring that each script 
-#  runs to completion before the next begins. 
-#  This program aims to streamline the 
-#  generation of outputs from all your 
-#  previous files, consolidating the 
-#  results into one sequence.
 # -----------------------------------------------
 
 import subprocess
