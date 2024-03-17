1f84495cb005b8b31b272a8bbfa248aa5557dd0e
Muen Zhang <75929368+zmuen@users.noreply.github.com>
2024-03-17 18:03:56 -0400

    Update 01_pull.py

--- 01_pull.py
+++ 01_pull.py
@@ -4,13 +4,6 @@
 #  Objective: Download all the public course 
 #  catalog data in raw HTML format from a 
 #  university website.
-# 
-#  Tools/Resources: Extract all the course 
-#  catalog data from one of the follow 
-#  three universities:
-#     Harvard: https://courses.my.harvard.edu
-#     BU: https://www.bu.edu/academics/cas/courses
-#     NE: https://catalog.northeastern.edu/course-descriptions
 # -----------------------------------------------
 
 import urllib.request
